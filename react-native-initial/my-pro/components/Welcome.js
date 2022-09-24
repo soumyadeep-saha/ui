3bfdfbaaa32dd16810dcfa8e8514cc125206563c
soumyadeep-saha <saha.soumyadeep90@gmail.com>
2022-09-24 21:53:55 +0530

    training

--- react-native-initial/my-pro/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import { Button, Text, View } from "react-native";
-
-export default function Welcome(props) {
-  return (
-    <View>
-      <Text>Welcome {props.name}</Text>
-    </View>
-  );
-}
--- react-native-initial/trip-manager/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { tripReducer } from "../slice/trips";
-
-export const store = configureStore({
-  reducer: {
-    trips: tripReducer,
-  },
-});
